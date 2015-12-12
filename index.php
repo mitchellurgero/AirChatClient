4c98448921de33d4f72cf67957c43bc0f1a7e708
mitchellurgero <mitchellurgero@gmail.com>
2015-12-12 00:43:08 -0600

    Attempt to fix logout bug.

--- index.php
+++ index.php
@@ -415,8 +415,8 @@ window.onbeforeunload = confirmExit;
   function confirmExit()
   {
     connection.disconnect();
-    jsxc.xmpp.disconnect();
-    //return "You have attempted to leave this page.  If you have made any changes to the fields without clicking the Save button, your changes will be lost.  Are you sure you want to exit this page?";
+    jsxc.xmpp.logout();
+    
   }
     </script>
   </head>
